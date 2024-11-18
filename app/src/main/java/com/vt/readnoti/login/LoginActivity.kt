178e1a40baeb77d969fe5a8fb32e55bab2e23290
Nguyen Tan <riptan2001@gmail.com>
2024-11-18 20:50:09 +0700

    add login with gg

--- app/src/main/java/com/vt/readnoti/login/LoginActivity.kt
+++ app/src/main/java/com/vt/readnoti/login/LoginActivity.kt
@@ -1,32 +1,38 @@
 package com.vt.readnoti.login
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.remoteConfig
 import com.google.firebase.remoteconfig.remoteConfigSettings
 import com.vt.readnoti.MainActivity
 import com.vt.readnoti.R
 import com.vt.readnoti.base.BaseActivity
+import com.vt.readnoti.databinding.ActivityLoginBinding
 import com.vt.readnoti.notification.Constraints
 
 class LoginActivity : BaseActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private var startMain = true
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -51,14 +57,49 @@ class LoginActivity : BaseActivity() {
                 if (task.isSuccessful) {
                     val updated = task.result
                     if(updated){
-                        val showScreen = remoteConfig.getBoolean(Constraints.SR)
-                        if(showScreen){
-                            start(MainActivity::class.java)
-                        }
+                         startMain = remoteConfig.getBoolean(Constraints.SR)
                     }
-
                 }
-
             }
+        binding.btnGoogle.setOnClickListener {
+            signIn()
+        }
+
+    }
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Handle error
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    val user = auth.currentUser
+                    if(user != null && startMain) {
+                        start(MainActivity::class.java)
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                }
+            }
+    }
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
---
+++ app/src/main/java/com/vt/readnoti/notification/Constraints.kt
@@ -0,0 +1,8 @@
+package com.vt.readnoti.notification
+
+class Constraints {
+    companion object {
+        const val NOTIFICATION_LISTENER_EXAMPLE = "com.vt.readnoti.NOTIFICATION_LISTENER_EXAMPLE"
+        const val SR = "success_auth"
+    }
+}
